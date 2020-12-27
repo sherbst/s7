c7617ef5567df205481696793efe546f3a62fc5b
Sawyer Herbst <contact@sawyerherbst.com>
2020-12-27 09:56:44 -0600

    Benchmark image

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,12 +3,16 @@ use s7::algorithm::encode::encode;
 use s7::read_png::read_png;
 
 fn benchmark(c: &mut Criterion) {
-    let image = read_png("input/input.png");
+    let image = read_png("benches/input.png");
 
     c.bench_function("get edge paths", |b| {
         b.iter_with_setup(|| image.clone(), |img| encode(img))
     });
 }
 
-criterion_group!(benches, benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = benchmark
+}
 criterion_main!(benches);
