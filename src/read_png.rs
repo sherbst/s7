cc628310974f824991d9bef8a63b38bc8d6ba41c
Sawyer Herbst <contact@sawyerherbst.com>
2020-12-28 08:53:28 -0600

    Rename

--- src/read_png.rs
+++ src/read_png.rs
@@ -3,7 +3,7 @@ use image::io::Reader as ImageReader;
 use image::{DynamicImage, ImageBuffer, Rgb, Rgba};
 use imageproc::map::map_colors;
 
-fn rgba_to_rgb(rgba_image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+fn strip_alpha(rgba_image: ImageBuffer<Rgba<u8>, Vec<u8>>) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
     map_colors(&rgba_image, |rgba| Rgb([rgba.0[0], rgba.0[1], rgba.0[2]]))
 }
 
@@ -12,7 +12,7 @@ pub fn read_png(path: &str) -> Image {
 
     match image {
         DynamicImage::ImageRgb8(rgb_image) => Image::new(rgb_image),
-        DynamicImage::ImageRgba8(rgba_image) => Image::new(rgba_to_rgb(rgba_image)),
+        DynamicImage::ImageRgba8(rgba_image) => Image::new(strip_alpha(rgba_image)),
         _ => panic!("Only accepts RGB and RGBA images for now"),
     }
 }
